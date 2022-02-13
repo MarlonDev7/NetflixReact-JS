74d803c91f67191ec07f8438078cf4cf2b0d4b5b
Marlon Amaral <progamaral@gmail.com>
2022-02-13 18:49:22 -0400

    first commit

---
+++ src/components/FeaturedMovie.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './FeaturedMovie.css';
+
+export default ({item}) =>{
+    return (
+        <section className='featured'>
+            <div>{item.original_name}</div>
+            
+        </section>
+    );
+}
